2d291ff83d98a932ab20f28d510b8227d548a9b0
DSSP <dssp@MacBook-Pro-de-Jean-Christophe.local>
2017-11-25 14:01:57 +0100

    updating JCS submission

--- submissions/JCS/regressor.py
+++ submissions/JCS/regressor.py
@@ -4,9 +4,7 @@ from xgboost import XGBRegressor
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = XGBRegressor(max_depth=8,learning_rate=0.02,n_estimators=400,subsample=0.8,colsample_bytree=1,min_child_weight=5)
-        # self.clf = RandomForestRegressor(n_estimators=50, max_depth=10, max_features=10)
-
+        self.clf = XGBRegressor(max_depth=15,learning_rate=0.02,n_estimators=1000,subsample=0.8,colsample_bytree=1,min_child_weight=5)
         
     def fit(self, X, y):
         self.clf.fit(X, y)
